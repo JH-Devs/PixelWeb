76b5e932230daf2c943f9d9ed2697c8f2e91162b
Jitka <info@jh-devs.cz>
2024-07-02 14:27:22 +0200

    add sessionFactory

---
+++ src/PixelWeb/Session/Exception/SessionStorageInvalidArgumentException.php
@@ -0,0 +1,12 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace PixelWeb\Session\Exception;
+
+use InvalidArgumentException;
+
+class SessionStorageInvalidArgumentException extends InvalidArgumentException
+{
+
+}
--- src/PixelWeb/Session/SessionFactory.php
+++ src/PixelWeb/Session/SessionFactory.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 
 namespace PixelWeb\Session;
 
+use PixelWeb\Session\Storage\SessionStorageInterface;
+use PixelWeb\Session\SessionInterface;
+use PixelWeb\Session\Exception\SessionStorageInvalidArgumentException;
+
 class SessionFactory
 {
-    
+    public function __construct()
+    {
+
+    }
+    public function create(string $sessionName, string $storageString, array $options = []) : SessionInterface
+    {
+        $storageObject = new storageString($options);
+        if (!$storageObject instanceof SessionStorageInterface) {
+            throw new SessionStorageInvalidArgumentException($storageString . ' není platný objekt úložiště relace.');
+        }
+        return new Session($sessionName, $storageObject);
+    }
+	
 }
